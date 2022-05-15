63aebe7502b59e6036be1c7a92aced54c3241d74
leantrim <leutrimbunjaku5@gmail.com>
2022-05-15 12:31:48 +0200

    finished banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { baseUrl } from '../constants/movie'
 import { Movie } from '../typings'
 import { FaPlay } from 'react-icons/fa'
+import { InformationCircleIcon } from '@heroicons/react/solid'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -33,16 +34,18 @@ function Banner({ netflixOriginals }: Props) {
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl">
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl">
         {movie?.overview}
       </p>
 
-      <div>
-        <button className="bannerButton">
+      <div className="flex space-x-3">
+        <button className="bannerButton bg-white text-black">
           <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />
           Play
         </button>
-        <button className="bannerButton">More Info</button>
+        <button className="bannerButton bg-[gray]/70">
+          More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+        </button>
       </div>
     </div>
   )

dd391c7e4e97e17209a6548de1b444efc4d4df8c
leantrim <leutrimbunjaku5@gmail.com>
2022-05-15 10:53:28 +0200

    added netfllix logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
         <title>Create Next A</title>
         <link rel="icon" href="/favicon.ico" />

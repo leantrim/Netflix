27123084d548e8830883dbfc19bf9b91fa02d901
leantrim <leutrimbunjaku5@gmail.com>
2022-05-15 11:14:45 +0200

    added profile pic and icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+
 function Header() {
   return (
     <header>
@@ -17,7 +20,14 @@ function Header() {
         </ul>
       </div>
 
-      <div></div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img src="https://rb.gy/g1pwyx" className="cursor-pointer rounded" />
+        </Link>
+      </div>
     </header>
   )
 }
